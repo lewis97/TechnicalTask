34b40ae8cb0532f95a33d720c685a4de37f9d3d6
lewis-scott-smith <lewis-scott.smith@hpe.com>
2024-06-02 13:04:28 +0100

    update db

--- internal/adapters/datastore/migrations/01_create_tables.sql
+++ internal/adapters/datastore/migrations/01_create_tables.sql
@@ -5,6 +5,18 @@ CREATE TABLE accounts (
     document_num INT NOT NULL
 );
 
+CREATE TYPE operation_type AS ENUM ('INVALID', 'CASH PURCHASE', 'INSTALLMENT PURCHASE', 'WITHDRAWAL', 'PAYMENT');
+
+CREATE TABLE transactions (
+    id TEXT PRIMARY KEY,
+    account_id TEXT NOT NULL
+    operation_id OPERATION_TYPE NOT NULL
+    amount INT NOT NULL
+    event_time TIMESTAMP NOT NULL
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+        ON UPDATE CASCADE
+);
+
 -- +migrate Down
 
 DROP TABLE accounts;
